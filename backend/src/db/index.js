0de2c0ba3eec3b7fa08aaa757da7956381124bf3
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-04-20 20:39:02 +0530

    Fix database connection URI to use MONGO_LIVE_URI

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const dbURI = `${process.env.MONGODB_URI}`;
+        const dbURI = `${process.env.MONGO_LIVE_URI}`;
         const connectionInstance = await mongoose.connect(dbURI);
         console.log(`MongoDB Connected at HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
